8a083155d7f5e172f5799f1576fbd4d50fc7cda7
user <user@user-PC>
2018-05-29 16:03:29 +0900

    네네ㅔ네네

---
+++ java_example_01/src/java_example_01/Charexample.java
@@ -0,0 +1,16 @@
+package java_example_01;
+
+public class Charexample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//문자형 변수 char
+		char c1 = 'A';
+		char c2 = 65;//아스키코드 값
+		char c3 = 97;
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(c3);
+	}
+
+}
---
+++ java_example_01/src/java_example_01/boolean_example.java
@@ -0,0 +1,21 @@
+package java_example_01;
+
+public class boolean_example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/*
+ * 불린타입(Boolean)변수는 true, false의 값만 가질 수 있음.
+ * true=1, false =0으로 인식
+ * 
+ */
+		//bool 타입의 stop 변수 선언.
+		boolean stop = false;
+		if(stop) {
+			System.out.println("중지합니다.");
+		}else {
+				System.out.println("시작합니다.dsds");
+			}
+		}
+	
+}
